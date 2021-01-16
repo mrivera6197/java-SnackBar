c797a9b4d221add97e4932acb8f25de25c9124ab
mrivera6197 <mrivera6197@gmail.com>
2021-01-16 14:31:22 -0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+package snackbar;
+
+import java.text.DecimalFormat;
+
+public class Main {
+  private static DecimalFormat df = new DecimalFormat("$#, ##0.00");
+
+  private static void main(String arg[]) {
+
+    // instantiate 2 customers
+    Customer jane = new Customer("Jane", 45.25);
+    Customer bob = new Customer("Bob", 33.14);
+
+    // instantiate 3 vending machines
+
+    // instantiate 5 snacks
+  }
+}
